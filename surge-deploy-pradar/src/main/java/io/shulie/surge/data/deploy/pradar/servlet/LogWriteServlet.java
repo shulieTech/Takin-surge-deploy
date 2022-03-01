d492cef074f546e0af8a31bff2cce42059c50da6
sunshiyu <sunshiyu@shulie.io>
2022-03-01 13:32:11 +0800

    feature:add log upload interface.

---
+++ surge-deploy-pradar/src/main/java/io/shulie/surge/data/deploy/pradar/servlet/LogWriteServlet.java
@@ -0,0 +1,45 @@
+package io.shulie.surge.data.deploy.pradar.servlet;
+
+import com.google.inject.Singleton;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Sunsy
+ * @date 2022/2/24
+ * @apiNode
+ * @email sunshiyu@shulie.io
+ */
+@Singleton
+public class LogWriteServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doHead(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("收到日志=======================");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.setContentType("application/json;charset=utf-8");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+}
