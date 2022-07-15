a4557d68a8001d4da1912b210148a48da3a77687
zhouyuan <zhouyuan@shulie.io>
2022-07-15 16:23:30 +0800

    边缓存使用分页去获取,数据太多导致查询慢

--- surge-deploy-pradar/src/main/java/io/shulie/surge/data/deploy/pradar/common/EagleLoader.java
+++ surge-deploy-pradar/src/main/java/io/shulie/surge/data/deploy/pradar/common/EagleLoader.java
@@ -8,6 +8,8 @@ import io.shulie.surge.data.sink.mysql.MysqlSupport;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
@@ -23,14 +25,17 @@ public class EagleLoader {
      */
     private Cache<String, String> cache = CacheBuilder.newBuilder().maximumSize(30000).expireAfterWrite(1, TimeUnit.HOURS).build();
 
+    private static int pageSize = 500;
+
     @Inject
     private MysqlSupport mysqlSupport;
 
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     private static final String QUERY_LINK_CONFIG = "select link_id from t_amdb_pradar_link_config";
 
-    private static final String QUERY_LINK_EDGE = "select edge_id from t_amdb_pradar_link_edge where link_id='%s' " +
-            " and gmt_modify >DATE_ADD(NOW(), INTERVAL -1440 MINUTE) " +
-            " order by gmt_modify desc";
+    private static final String QUERY_LINK_EDGE = "select edge_id from t_amdb_pradar_link_edge where link_id='%s' and gmt_modify > '%s' limit %d,%d";
+
 
     public void init() {
         Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> initConfig(), 0, 5, TimeUnit.MINUTES);
@@ -38,17 +43,31 @@ public class EagleLoader {
 
     private void initConfig() {
         try {
+            // 查询前一天
+            LocalDateTime localDateTime = LocalDateTime.now().minusMinutes(1440);
+            String startDate = dateTimeFormatter.format(localDateTime);
             // 获取所有链路配置
             List<Map<String, Object>> linkConfigList = mysqlSupport.queryForList(QUERY_LINK_CONFIG);
             if (CollectionUtils.isNotEmpty(linkConfigList)) {
                 linkConfigList.stream().forEach(linkConfig -> {
                     Object linkId = linkConfig.get("link_id");
-                    List<Map<String, Object>> edgeList = mysqlSupport.queryForList(String.format(QUERY_LINK_EDGE, linkId));
-                    if (CollectionUtils.isNotEmpty(edgeList)) {
-                        edgeList.stream().forEach(edge -> {
-                            String edgeId = String.valueOf(edge.get("edge_id"));
-                            this.cache.put(edgeId, edgeId);
-                        });
+                    // 分页去查询,每次查询1000条数据
+                    int startIndex = 1;
+                    while (true) {
+                        // 计算偏移量
+                        int offset = (startIndex - 1) * pageSize;
+                        String querySql = String.format(QUERY_LINK_EDGE, linkId, startDate, offset, pageSize);
+                        List<Map<String, Object>> edgeList = mysqlSupport.queryForList(querySql);
+                        if (CollectionUtils.isNotEmpty(edgeList)) {
+                            edgeList.stream().forEach(edge -> {
+                                String edgeId = String.valueOf(edge.get("edge_id"));
+                                this.cache.put(edgeId, edgeId);
+                            });
+                            startIndex++;
+                        } else {
+                            // 遍历完成
+                            break;
+                        }
                     }
                 });
             }
