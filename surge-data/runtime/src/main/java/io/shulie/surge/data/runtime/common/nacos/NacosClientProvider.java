099fc74a9a861095a3f6a15284082a55b92caa0b
李学森 <lixs151@chinaunicom.cn>
2023-01-17 23:25:23 +0800

    适配nacos密码认证

--- surge-data/runtime/src/main/java/io/shulie/surge/data/runtime/common/nacos/NacosClientProvider.java
+++ surge-data/runtime/src/main/java/io/shulie/surge/data/runtime/common/nacos/NacosClientProvider.java
@@ -17,14 +17,22 @@ public class NacosClientProvider implements Provider<ConfigService> {
     private ConfigService configService = null;
 
     @Inject
-    public NacosClientProvider(@Named("nacos.server.addr") String nacosServerAddr) {
+    public NacosClientProvider(@Named("nacos.server.addr") String nacosServerAddr,
+                               @Named("nacos.namespace") String nacosNamespace,
+                               @Named("nacos.username") String nacosUsername,
+                               @Named("nacos.password") String nacosPassword) {
 
         try {
             Properties properties = new Properties();
             properties.put(PropertyKeyConst.SERVER_ADDR, nacosServerAddr);
+            properties.put(PropertyKeyConst.NAMESPACE, nacosNamespace);
+            properties.put(PropertyKeyConst.USERNAME, nacosUsername);
+            properties.put(PropertyKeyConst.PASSWORD, nacosPassword);
+
             configService = ConfigFactory.createConfigService(properties);
+
         } catch (Exception e) {
-            logger.error("init nacos client error.", e);
+            logger.error("Failed to initialize nacos client. {}", e.toString());
         }
     }
 
