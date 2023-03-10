815dc8197e642f447506a3cd2766f30e94bd44fe
李学森 <lixs151@chinaunicom.cn>
2023-03-10 12:01:56 +0800

    测试环境切换到新集群配置

---
+++ kafka-receiver/surge-deploy-kafka-receiver/configurationFiles/test/xixian/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#JAVA_OPTS=\
+#"-javaagent:/data/apps/takin-amdb/pinpoint-agent-2.0.3-origin/pinpoint-bootstrap.jar \
+#-Dpinpoint.agentId=${HOSTNAME:0-16} \
+#-Dpinpoint.applicationName=stresstest_cloud_xixian_test \
+#-Dpinpoint.licence=1d07fa023d02d60d \
+#-Duser.timezone=Asia/Shanghai"
+
+JAVA_OPTS=\
+"-Duser.timezone=Asia/Shanghai \
+-Xms3g \
+-Xmx3g"
+
+java ${JAVA_OPTS} -Dkafka.sdk.properties.path=/data/apps/kafka-receiver/config/kafka.properties \
+-jar ./takin-kafka-receiver.jar \
+--properties ./config/application.yml
