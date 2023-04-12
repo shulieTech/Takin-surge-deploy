f2daaa891efd77ef7f83a014d618bcc2a4bf3370
崔巍睿 <cuiwr@chinaunicom.cn>
2023-04-12 18:23:10 +0800

    自监控开启

--- configurationFiles/prod/xixian/start-surge-link.sh
+++ configurationFiles/prod/xixian/start-surge-link.sh
@@ -1,16 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-#JAVA_OPTS=\
-#"-javaagent:/data/apps/takin-amdb/pinpoint-agent-2.0.3-origin/pinpoint-bootstrap.jar \
-#-Dpinpoint.agentId=${HOSTNAME:0-16} \
-#-Dpinpoint.applicationName=stresstest_cloud_xixian_test \
-#-Dpinpoint.licence=1d07fa023d02d60d \
-#-Duser.timezone=Asia/Shanghai"
+dir=$PWD
+hostName=${HOSTNAME:0:16}
 
 JAVA_OPTS=\
-"-Duser.timezone=Asia/Shanghai \
+"-javaagent:$dir/pinpoint-agent-2.0.3-origin/pinpoint-bootstrap.jar \
+-Dpinpoint.agentId=$hostName \
+-Dpinpoint.applicationName=pressure_surge_link_xixian_prod \
+-Dpinpoint.licence=4AHVKh0zZIp3DLkE \
+-Dpinpoint.log=$dir/pinpoint-agent-2.0.3-origin/ \
+-Duser.timezone=Asia/Shanghai \
 -Xms3g \
 -Xmx3g"
+
+# JAVA_OPTS=\
+# "-Duser.timezone=Asia/Shanghai \
+# -Xms3g \
+# -Xmx3g"
 
 java ${JAVA_OPTS} -classpath takin-surge.jar \
 -Dswitcher.link.task=true \
