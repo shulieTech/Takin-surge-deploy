4f9ffc95534640eac7b38f19d947d40bd5edac5a
xiaobin <hiloo123@126.com>
2023-04-13 17:08:29 +0800

    修改参数转换类实现

--- surge-data/sink/mysql/src/main/java/io/shulie/surge/data/sink/mysql/MysqlSupport.java
+++ surge-data/sink/mysql/src/main/java/io/shulie/surge/data/sink/mysql/MysqlSupport.java
@@ -166,9 +166,9 @@ public class MysqlSupport implements Lifecycle, Stoppable {
      */
 
     public void addBatch(final String sql, final List<Object[]> batchArgs) {
-        RotationBatch<Object[]> rotationBatch = null;
-        if (!rotationPrepareSqlBatch.containsKey(sql)) {
-            rotationBatch = new RotationBatch(new CountRotationPolicy(200), new TimedRotationPolicy(2, TimeUnit.SECONDS));
+        RotationBatch<Object[]> rotationBatch = rotationPrepareSqlBatch.get(sql);
+        if (rotationBatch == null) {
+            rotationBatch = new RotationBatch<>(new CountRotationPolicy(200), new TimedRotationPolicy(2, TimeUnit.SECONDS));
             rotationBatch.batchSaver(new RotationBatch.BatchSaver<Object[]>() {
                 @Override
                 public boolean saveBatch(LinkedBlockingQueue<Object[]> batchSql) {
@@ -181,13 +181,16 @@ public class MysqlSupport implements Lifecycle, Stoppable {
                     return false;
                 }
             });
-        } else {
-            rotationBatch = rotationPrepareSqlBatch.get(sql);
+            RotationBatch old = rotationPrepareSqlBatch.putIfAbsent(sql, rotationBatch);
+            if (old != null) {
+                RotationBatch current = rotationBatch;
+                rotationBatch = old;
+                current.stop();
+            } else {
+                rotationBatch.start();
+            }
         }
-        for (Object[] args : batchArgs) {
-            rotationBatch.addBatch(args);
-        }
-        rotationPrepareSqlBatch.put(sql, rotationBatch);
+        rotationBatch.addBatch(batchArgs);
     }
 
     public void updateBatch(final String sql, final List<Object[]> batchArgs) {
