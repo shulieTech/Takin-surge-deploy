a92d1b499083d9e711068d8c02dcfce7e1a3c49b
zhangya <zhangya@shulie.io>
2023-12-12 09:52:37 +0800

    调整依赖包版本

--- surge-data/sink/kafka/src/main/java/io/shulie/surge/data/sink/kafka/DefaultKafkaSupport.java
+++ surge-data/sink/kafka/src/main/java/io/shulie/surge/data/sink/kafka/DefaultKafkaSupport.java
@@ -56,7 +56,6 @@ public class DefaultKafkaSupport implements KafkaSupport {
      *
      * @param topic
      * @param msg          发送消息
-     * @param sendCallback
      * @throws Exception
      */
     @Override
@@ -68,7 +67,6 @@ public class DefaultKafkaSupport implements KafkaSupport {
      * 单向发送
      *
      * @param topic
-     * @param tag
      * @param msg   发送消息
      * @throws Exception
      */
