4135b190a772c0014d049964e72aa7a54d1b6f6b
zxzghr <tian_ya_1990@126.com>
2024-02-27 14:17:39 +0800

    edage恢复-老逻辑

--- surge-deploy-pradar/src/main/java/io/shulie/surge/data/deploy/pradar/common/EagleLoader.java
+++ surge-deploy-pradar/src/main/java/io/shulie/surge/data/deploy/pradar/common/EagleLoader.java
@@ -7,12 +7,7 @@ import com.google.inject.Singleton;
 import io.shulie.surge.data.sink.mysql.MysqlSupport;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
@@ -23,25 +18,19 @@ import java.util.concurrent.TimeUnit;
  */
 @Singleton
 public class EagleLoader {
-    private static Logger logger = LoggerFactory.getLogger(EagleLoader.class);
-
-    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
     /**
      * 边缓存
      */
-    private Cache<String, String> cache = CacheBuilder.newBuilder().maximumSize(60000).expireAfterWrite(1, TimeUnit.HOURS).build();
+    private Cache<String, String> cache = CacheBuilder.newBuilder().maximumSize(30000).expireAfterWrite(1, TimeUnit.HOURS).build();
 
     @Inject
     private MysqlSupport mysqlSupport;
 
     private static final String QUERY_LINK_CONFIG = "select link_id from t_amdb_pradar_link_config";
 
-    private static final String QUERY_LINK_EDGE = "select edge_id from t_amdb_pradar_link_edge where link_id='%s' order by id desc limit %d";
-
-    private static final String QUERY_LINK_EDGE_COUNT = "select count(*) from t_amdb_pradar_link_edge where link_id='%s'";
-
-    private static int pageSize = 1000;
+    private static final String QUERY_LINK_EDGE = "select edge_id from t_amdb_pradar_link_edge where link_id='%s' " +
+            " and gmt_modify >DATE_ADD(NOW(), INTERVAL -1440 MINUTE) " +
+            " order by gmt_modify desc limit 2000";
 
     public void init() {
         Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> initConfig(), 0, 5, TimeUnit.MINUTES);
@@ -54,24 +43,17 @@ public class EagleLoader {
             if (CollectionUtils.isNotEmpty(linkConfigList)) {
                 linkConfigList.stream().forEach(linkConfig -> {
                     Object linkId = linkConfig.get("link_id");
-                    String countSql = String.format(QUERY_LINK_EDGE_COUNT, linkConfig);
-                    Integer count = mysqlSupport.queryForObject(countSql, Integer.class);
-                    if (count > pageSize) {
-                        logger.warn("当前{}对应的边已超1000,请检查！！！", linkId);
-                    }
-                    String querySql = String.format(QUERY_LINK_EDGE, linkId, pageSize);
-                    List<Map<String, Object>> edgeList = mysqlSupport.queryForList(querySql);
+                    List<Map<String, Object>> edgeList = mysqlSupport.queryForList(String.format(QUERY_LINK_EDGE, linkId));
                     if (CollectionUtils.isNotEmpty(edgeList)) {
                         edgeList.stream().forEach(edge -> {
                             String edgeId = String.valueOf(edge.get("edge_id"));
-                            // value设置为空
-                            this.cache.put(edgeId, "");
+                            this.cache.put(edgeId, edgeId);
                         });
                     }
                 });
             }
         } catch (Throwable e) {
-            logger.warn("链路边缓存异常," + ExceptionUtils.getStackTrace(e));
+            e.printStackTrace();
         }
     }
 
